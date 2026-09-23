d70ec3cd672aea5e1432c1912f44866a05eeb564
Jessel Sookha <jsookha@gmail.com>
2026-09-23 12:11:50 +0200

    stage 2 - p1 intro game screen

--- App.tsx
+++ App.tsx
@@ -1,7 +1,25 @@
 import { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
+// NavigationContainer wraps the whole app and manages navigation state
+import { NavigationContainer } from '@react-navigation/native';
+// createNativeStackNavigator builds a stack-based navigator using native transitions
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={PlayerScreen} />
+        <Stack.Screen name="Game" component={GameScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function PlayerScreen({ navigation }: any){
   const [player1, setPlayer1] = useState<string>('');
   const [player2, setPlayer2] = useState<string>('');
 
@@ -24,8 +42,16 @@ export default function App() {
 
       <Button
         title="Start Game"
-        onPress={() => console.log(`Player 1: ${player1}, Player 2: ${player2}`)}
+        onPress={() => navigation.navigate('Game')}
       />
+    </View>
+  );
+}
+
+function GameScreen({ navigation, route} : any){
+  return(
+    <View style={styles.container}>
+      <Text style={styles.title}>Player 1 vs. Player 2</Text>
     </View>
   );
 }
